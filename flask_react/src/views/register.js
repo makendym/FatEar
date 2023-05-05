61c5b350c089b2bade0d23596ae43c1c8fc7ab24
makendym <mmidouin18@stac.edu>
2023-05-05 16:27:34 -0400

    fixed register form and addded error handling

--- flask_react/src/views/register.js
+++ flask_react/src/views/register.js
@@ -15,6 +15,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
 
 function Copyright(props) {
   return (
@@ -89,118 +90,132 @@ function Register() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 3 }}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="fname"
-                  label="First Name"
-                  name="fname"
-                  value={fname}
-                  onChange={(event) => setFname(event.target.value)}
-                />
-                <TextField
-                  required
-                  fullWidth
-                  id="lname"
-                  label="Last Name"
-                  name="lname"
-                  value={lname}
-                  onChange={(event) => setLname(event.target.value)}
-                />
-                <TextField
-                  required
-                  fullWidth
-                  id="username"
-                  label="Username"
-                  name="username"
-                  value={username}
-                  onChange={(event) => setUsername(event.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="pwd"
-                  label="Password"
-                  type="password"
-                  id="pwd"
-                  autoComplete="new-password"
-                  value={pwd}
-                  onChange={(event) => setPwd(event.target.value)}
-                />
-                <TextField
-                  required
-                  fullWidth
-                  id="nickname"
-                  label="Nickname"
-                  name="nickname"
-                  value={nickname}
-                  onChange={(event) => setNickname(event.target.value)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value="allowExtraEmails" color="primary" />
-                  }
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            {error && (
-              <p className="error">
-                <strong>Error:</strong> {error}
-              </p>
-            )}
-            {success && (
-              <p className="success">
-                <strong>Success:</strong> {success}
-              </p>
-            )}
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
+<Container component="main" maxWidth="xs">
+  <CssBaseline />
+  <Box
+    sx={{
+      marginTop: 8,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    }}
+  >
+    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+      <LockOutlinedIcon />
+    </Avatar>
+    <Typography component="h1" variant="h5">
+      Sign up
+    </Typography>
+    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            autoComplete="fname"
+            name="firstName"
+            required
+            fullWidth
+            id="firstName"
+            label="First Name"
+            autoFocus
+            value={fname}
+            onChange={(event) => setFname(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            required
+            fullWidth
+            id="lastName"
+            label="Last Name"
+            name="lastName"
+            autoComplete="lname"
+            value={lname}
+            onChange={(event) => setLname(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            name="pwd"
+            label="Password"
+            type="password"
+            id="pwd"
+            autoComplete="new-password"
+            value={pwd}
+            onChange={(event) => setPwd(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            required
+            fullWidth
+            id="nickname"
+            label="Nickname"
+            name="nickname"
+            autoComplete="nickname"
+            value={nickname}
+            onChange={(event) => setNickname(event.target.value)}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <FormControlLabel
+            control={<Checkbox value="allowExtraEmails" color="primary" />}
+            label="I want to receive inspiration, marketing promotions and updates via email."
+          />
+        </Grid>
+      </Grid>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Sign Up
+      </Button>
+      {error && (
+        <p className="error">
+          <strong>Error:</strong> {error}
+        </p>
+      )}
+      {success && (
+        <p className="success">
+          <strong>Success:</strong> {success}
+        </p>
+      )}
+      <Grid container justifyContent="flex-end">
+        <Grid item>
+          <Link href="#" variant="body2">
+            Already have an account? Sign in
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  </Box>
+  <Box mt={8}>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"© "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  </Box>
+</Container>
+
     </ThemeProvider>
   );
 }
